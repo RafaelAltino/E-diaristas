d8074808c66921d8d7e87c85e4856f2329a18421
Rafael Felipe Reis Altino <rafaelfraltino@gmail.com>
2021-06-15 16:31:32 -0300

    iniciando a criação do campo SafeEnvironment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
+
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <SafeEnvironment />
+    </div>
+  )
 }
---
+++ src/ui/components/feedback/SafeEnvironment/SafeEnvironment.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SafeEnvironment = () => {
+    return <div>Ambiente Seguro</div>
+}
+
+export default SafeEnvironment;
